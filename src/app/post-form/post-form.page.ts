cc921c9cfde39388c197de73460276a1397d33b2
DESKTOP-311EKJF\andrew <ramirezandrew172@gmail.com>
2023-02-25 04:22:06 -0500

    Initial Commit

---
+++ src/app/post-form/post-form.page.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from "@angular/core";
+import { PostService, Post } from "../services/post.service";
+import { Router, ActivatedRoute } from "@angular/router";
+
+@Component({
+  selector: "app-post-form",
+  templateUrl: "./post-form.page.html",
+  styleUrls: ["./post-form.page.scss"],
+})
+export class PostFormPage implements OnInit {
+  post: Post = {
+    name: "",
+    email: "",
+    password: "",
+  };
+
+  editing = false;
+
+  constructor(
+    private postService: PostService,
+    private router: Router,
+    private actiavtedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.actiavtedRoute.paramMap.subscribe((paramMap) => {
+      if (paramMap.get("postId")) {
+        this.postService
+          .getPostById(paramMap.get("postId"))
+          .subscribe((res) => {
+            this.post = res;
+            this.editing = true;
+          });
+      }
+    });
+  }
+
+  savePost() {
+    this.postService
+      .createPost(this.post.name, this.post.email, this.post.password)
+      .subscribe((res) => {
+        console.log(res);
+        this.router.navigate(["/posts"]);
+      });
+  }
+
+  updatePost() {
+    this.postService
+      .updatePost(this.post.id, {
+        name: this.post.name,
+        email: this.post.email,
+        password: this.post.password
+      })
+      .subscribe((res) => {
+        console.log(res);
+        this.editing = false;
+        this.router.navigate(["/posts"]);
+      });
+  }
+}
---
+++ src/app/services/post.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { environment } from "../../environments/environment";
+
+export interface Post {
+  id?: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+@Injectable({
+  providedIn: "root",
+})
+export class PostService {
+  constructor(private http: HttpClient) {}
+
+  API = `${environment.API}/posts`;
+
+  getPosts() {
+    return this.http.get<Post[]>(`${this.API}/users`);
+  }
+
+  createPost(name: string, email: string, password: string) {
+    return this.http.post<Post[]>(`${this.API}/`, {
+      name,
+      email,
+      password
+    });
+  }
+
+  removePost(id: string) {
+    return this.http.delete<Post>(`${this.API}/${id}`);
+  }
+
+  getPostById(id: string) {
+    return this.http.get<Post>(`${this.API}/users/${id}`);
+  }
+
+  updatePost(id: string, post: Post) {
+    return this.http.put(`${this.API}/users/${id}`, post);
+  }
+}
